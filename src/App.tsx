19dca22850a48ab83fff21f48c47085361b293b3
gabrielrosa2705@gmail.com <gabrielrosa2705@gmail.com>
2023-03-22 22:06:19 -0300

    envio da atividade

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Rotas } from './routes/Rotas';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Galeria de fotos</h1>
+            <hr />
+            <Rotas />
+        </div>
+    );
+}
+
+export default App;
